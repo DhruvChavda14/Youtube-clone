6dc8e8680201ec8da62f106db84b18caa5a25570
Dhruv Chavda <dhruvchavda@192.168.1.7>
2024-07-08 18:32:19 +0530

    Updated url

--- client/src/Components/ShowVideo/ShowVideo.jsx
+++ client/src/Components/ShowVideo/ShowVideo.jsx
@@ -8,7 +8,7 @@ function ShowVideo({vid}) {
     <>
      <Link to={`/videopage/${vid?._id}`}>
         <video 
-        src={`http://localhost:5500/${vid.filePath}`}
+          src={`https://youtubeclone1403.netlify.app/${vid.filePath}`}
         // src={`https://youtubeclone5031.herokuapp.com/${vid.filePath}`}
         className="video_ShowVideo"
         />
